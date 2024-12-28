1c8532b7675ceea17962215caca3fb0a81939e6f
David <dmikalova@Davids-MacBook-Pro.local>
2024-12-27 17:00:29 -0800

    feat: add symlink script

---
+++ scripts/symlink.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -s "$HOME/Library/Application Support/factorio/mods/mklv-lib" "$HOME/Code/github.com/dmikalova/factorio-mods/mklv-lib"
+ln -s "$HOME/Library/Application Support/factorio/mods/mklv-quality-wagon" "$HOME/Code/github.com/dmikalova/factorio-mods/quality-wagon"
